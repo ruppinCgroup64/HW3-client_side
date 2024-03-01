e2928908d8ebd584e954cad1d47b025dc2b0aba5
Adi Hadad <agroconnect2024@gmail.com>
2024-03-01 14:18:20 +0200

    .

---
+++ HW3_part_2_cgroup64/HW3_cgroup64/BL/Flat.cs
@@ -0,0 +1,59 @@
+﻿namespace HW2_cgroup64.BL
+{
+    public class Flat
+    {
+        int id;
+        string city;
+        string address;
+        int numOfRooms;
+        double price;
+        static List<Flat> FlatsList = new List<Flat>();
+
+        public int Id { get => id; set => id = value; }
+        public string City { get => city; set => city = value; }
+        public string Address { get => address; set => address = value; }
+        public int NumOfRooms { get => numOfRooms; set => numOfRooms = value; }
+        public double Price { get => price; set => price = Discount(value); }
+
+
+        public Flat(int id, string city, string address, double price, int numOfRooms)
+        {
+            Id = id;
+            City = city;
+            Address = address;
+            NumOfRooms = numOfRooms;
+            Price = price;
+        }
+        public Flat() { }
+
+        public int Insert()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.InsertFlat(this);
+        }
+        public List<Flat> Read()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.ReadFlats();
+        }
+
+        public double Discount(double val)
+        {
+            if (numOfRooms > 1 && val > 100)
+                return val * 0.9;
+            return val;
+        }
+
+        //HW1
+        static public List<Flat> ReadByMaxAndCity(string city, double price)//return flats in city and under price
+        {
+            List<Flat> selectedFlats = new List<Flat>();
+            foreach (Flat f in FlatsList)
+            {
+                if (f.city == city && f.price <= price)
+                    selectedFlats.Add(f);
+            }
+            return selectedFlats;
+        }
+    }
+}
---
+++ HW3_part_2_cgroup64/HW3_cgroup64/BL/User.cs
@@ -0,0 +1,59 @@
+﻿using HW2_cgroup64.BL;
+
+namespace HW3_cgroup64.BL
+{
+    public class User
+    {
+        string firstName;
+        string familyName;
+        string email;
+        string password;
+        bool isActive;
+        bool isAdmin;
+        //static List<User> UsersList = new List<User>();
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string FamilyName { get => familyName; set => familyName = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public bool IsActive { get => isActive; set => isActive = value; }
+        public bool IsAdmin { get => isAdmin; set => isAdmin = value; }
+
+        public User(string firstName, string familyName, string email, string password, bool isActive, bool isAdmin)
+        {
+            this.FirstName = firstName;
+            this.FamilyName = familyName;
+            this.Email = email;
+            this.Password = password;
+            this.IsActive = isActive;
+            this.IsAdmin = isAdmin;
+        }
+        public User() { }
+
+        public int Insert()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.InsertUser(this);
+        }
+        public User LoginUser()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.Login(this);
+        }
+        public List<User> Read()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.ReadUsers();
+        }
+        public User Update() { 
+            DBservices dbs = new DBservices();
+            return dbs.UpdateUser(this);
+        }
+        public int UpdateIsActive()
+        {
+            DBservices dbs = new DBservices();
+            return dbs.UpdateUserIsActive(this);
+        }
+
+    }
+}
